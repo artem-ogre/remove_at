22e7ec8aa438252f1d5440d4b31837121e620ec4
Artem Amirkhanov <artem.ogre@gmail.com>
2018-08-13 11:03:53 +0200

    Fix a typo: 'fist' => 'first'

--- remove_at.hpp
+++ remove_at.hpp
@@ -16,21 +16,21 @@ inline ForwardIt remove_at(ForwardIt first, ForwardIt last, const size_t index)
 
 /*!
  * Remove elements in the range [first; last) with indices from the sorted
- * range [indices_fist, indices_last)
+ * range [indices_first, indices_last)
  */
 template <typename ForwardIt, typename SortedIndicesForwardIt>
 inline ForwardIt remove_at(
     ForwardIt first,
     ForwardIt last,
-    SortedIndicesForwardIt indices_fist,
+    SortedIndicesForwardIt indices_first,
     SortedIndicesForwardIt indices_last)
 {
     typedef typename std::vector<bool> Flags;
     // flag elements to keep
     Flags is_keep(
         static_cast<Flags::size_type>(std::distance(first, last)), true);
-    for(; indices_fist != indices_last; ++indices_fist)
-        is_keep[static_cast<Flags::size_type>(*indices_fist)] = false;
+    for(; indices_first != indices_last; ++indices_first)
+        is_keep[static_cast<Flags::size_type>(*indices_first)] = false;
     // move kept elements to beginning
     ForwardIt result = first;
     for(Flags::const_iterator keepIt = is_keep.begin(); first != last;
